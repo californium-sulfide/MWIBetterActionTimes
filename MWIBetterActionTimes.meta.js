7b7f14bf39a394b25cb9d5ac9933be8a1e6cc378
californium-sulfide <148061589+californium-sulfide@users.noreply.github.com>
2025-05-02 00:40:51 +0800

    Create MWIBetterActionTimes.meta.js

---
+++ MWIBetterActionTimes.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         MWIBetterActionTimes
+// @namespace    http://tampermonkey.net/
+// @version      2025-05-02-1
+// @description  More buttons to change action times
+// @author       Californium Sulfide
+// @match        https://www.milkywayidle.com/*
+// @match        https://test.milkywayidle.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=milkywayidle.com
+// @grant        none
+// @downloadURL https://github.com/californium-sulfide/MWIBetterActionTimes/raw/refs/heads/main/MWIBetterActionTimes.user.js
+// @updateURL https://github.com/californium-sulfide/MWIBetterActionTimes/raw/refs/heads/main/MWIBetterActionTimes.meta.js
+// ==/UserScript==
